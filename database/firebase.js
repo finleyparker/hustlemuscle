8f1aecfc1d01d81a04a85799824b059d70f55b18
jacobmoneda <104470514+jacobmoneda@users.noreply.github.com>
2025-05-14 12:56:45 +1200

    merge fix FINAL

--- database/firebase.js
+++ database/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -11,14 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export default app; const auth = getAuth(app)
-
-const db = initializeFirestore(app, {
-experimentalForceLongPolling: true
-})
-
-export { db, auth };
